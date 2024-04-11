da8b6472b5c9f1e0a150d3f0b51e24f0f0964939
Eduardo Yuschuk <eduardo.yuschuk@gmail.com>
2024-04-11 14:31:50 -0300

    stack allocated and shift

--- src/lib.rs
+++ src/lib.rs
@@ -1,28 +1,41 @@
-// struct _256 {
-//     bytes: [u8; 32]
-// }
-
-// struct __256<'a> {
-//     bytes: &'a [u8]
-// }
+use std::fmt;
 
 pub struct Uint256 {
-    bytes: Box<[u8]>,
+    bytes: [u8; 32],
+}
+
+impl fmt::Display for Uint256 {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let mut str = String::new();
+        for byte in self.bytes.iter() {
+            let v = format!("{:02x}", byte);
+            let a = v.as_str();
+            str = a.to_owned() + &str;
+        }
+        write!(f, "0x{}", str)
+    }
 }
 
 impl Uint256 {
+    const NUM_BYTES: usize = 32;
+
     pub fn zero() -> Self {
-        Uint256 {
-            bytes: Box::new([0_u8; 32]),
-        }
+        Uint256 { bytes: [0_u8; 32] }
     }
 
     pub fn one() -> Self {
         let mut bytes = [0_u8; 32];
         bytes[0] = 1_u8;
-        Uint256 {
-            bytes: Box::new(bytes),
+        Uint256 { bytes }
+    }
+
+    pub fn from_u64(value: u64) -> Self {
+        let mut bytes = [0_u8; 32];
+        let value_bytes = value.to_be_bytes();
+        for i in 0..8 {
+            bytes[i] = value_bytes[i];
         }
+        Uint256 { bytes }
     }
 
     pub fn get_byte(&self, index: usize) -> u8 {
@@ -30,7 +43,30 @@ impl Uint256 {
     }
 
     pub fn shift_left(&mut self, places: usize) {
-        self.bytes[0] <<= places;
+        let byte_shift = places / 8;
+        let bit_shift = places % 8;
+
+        if byte_shift > 0 {
+            let mut i = Self::NUM_BYTES - 1;
+            while i >= byte_shift {
+                self.bytes[i] = self.bytes[i - byte_shift];
+                i -= 1;
+            }
+
+            for i in 0..byte_shift {
+                self.bytes[i] = 0;
+            }
+        }
+
+        if bit_shift > 0 {
+            let mut i = Self::NUM_BYTES - 1;
+            while i > 0 {
+                self.bytes[i] =
+                    (self.bytes[i] << bit_shift) | (self.bytes[i - 1] >> (8 - bit_shift));
+                i -= 1;
+            }
+            self.bytes[0] <<= bit_shift;
+        }
     }
 }
 
@@ -41,37 +77,69 @@ mod tests {
     #[test]
     fn zero() {
         let zero = Uint256::zero();
-        for index in 0_usize..32_usize {
-            assert_eq!(zero.get_byte(index), 0_u8);
-        }
+        assert_eq!(
+            zero.to_string(),
+            "0x0000000000000000000000000000000000000000000000000000000000000000"
+        );
     }
 
     #[test]
     fn one() {
         let one = Uint256::one();
-        assert_eq!(one.get_byte(0), 1_u8);
-        for index in 1_usize..32_usize {
-            assert_eq!(one.get_byte(index), 0_u8);
-        }
+        assert_eq!(
+            one.to_string(),
+            "0x0000000000000000000000000000000000000000000000000000000000000001"
+        );
     }
 
     #[test]
-    fn shift_left_by_one() {
+    fn from_u64() {
+        assert_eq!(
+            Uint256::from_u64(u64::MAX).to_string(),
+            "0x000000000000000000000000000000000000000000000000ffffffffffffffff"
+        );
+    }
+
+    #[test]
+    fn shift_left() {
+        // shifting one
+
         let mut one = Uint256::one();
+        assert_eq!(
+            one.to_string(),
+            "0x0000000000000000000000000000000000000000000000000000000000000001"
+        );
+
         one.shift_left(1);
-        assert_eq!(one.get_byte(0), 0b00000010_u8);
-        for index in 1_usize..32_usize {
-            assert_eq!(one.get_byte(index), 0_u8);
-        }
-    }
+        assert_eq!(
+            one.to_string(),
+            "0x0000000000000000000000000000000000000000000000000000000000000002"
+        );
 
-    #[test]
-    fn shift_left_by_two() {
-        let mut one = Uint256::one();
-        one.shift_left(2);
-        assert_eq!(one.get_byte(0), 0b00000100_u8);
-        for index in 1_usize..32_usize {
-            assert_eq!(one.get_byte(index), 0_u8);
-        }
+        one.shift_left(7);
+        assert_eq!(
+            one.to_string(),
+            "0x0000000000000000000000000000000000000000000000000000000000000100"
+        );
+
+        one.shift_left(16);
+        assert_eq!(
+            one.to_string(),
+            "0x0000000000000000000000000000000000000000000000000000000001000000"
+        );
+
+        // shifting zero
+
+        let mut zero = Uint256::zero();
+        assert_eq!(
+            zero.to_string(),
+            "0x0000000000000000000000000000000000000000000000000000000000000000"
+        );
+
+        zero.shift_left(128);
+        assert_eq!(
+            zero.to_string(),
+            "0x0000000000000000000000000000000000000000000000000000000000000000"
+        );
     }
 }
